bab501fe27ce0eff2e3f564e776e6faac69c3b5b
Lay Tzeh Wu <laytzehwu@yahoo.com>
2021-07-30 21:56:17 +0800

    Apply sync of Rest Api call

--- src/services/weather/weather-service.js
+++ src/services/weather/weather-service.js
@@ -1,6 +1,6 @@
 
 const https = require('https');
-const Logger = require('../logger/logger');
+const logger = require('../logger');
 
 const apiKey = '7fac81b2676df4ebf79dabd8759a9ce7';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
@@ -13,29 +13,41 @@ class WeatherService {
         return path.replace(WeatherService.BASE_PATH, '');
     }
 
-    handleGet(req, res) {
-        const [empty, place] = this.getSubPath(req.url).split('/');
-        const url = `${baseUrl}?q=${place}&&appid=${apiKey}`;
-        const currentService = this;        
-        https.get(url, (apiRes) => {
-            apiRes.on('data', (d) => {
-                res.writeHead(apiRes.statusCode);
-                res.write(d);
-                res.end();
+    callApi(place) {
+        return new Promise((resolve, reject) => {
+            const url = `${baseUrl}?q=${place}&&appid=${apiKey}`;
+            https.get(url, (apiRes) => {
+                apiRes.on('data', (d) => {
+                    resolve({
+                        'apiRes': apiRes,
+                        'data': d + ""
+                    });
+                });
+            })
+            .on('error', (err) => {
+              // Check if retry is needed
+              if (req.reusedSocket && err.code === 'ECONNRESET') {
+                currentService.handleGet(req, res);
+              } else {
+                reject(err);
+              }
             });
-        })
-        .on('error', (err) => {
-          // Check if retry is needed
-          if (req.reusedSocket && err.code === 'ECONNRESET') {
-            currentService.handleGet(req,res);
-          } else {
-            Logger.log(`error ${err}`);
+        });
+    }
+
+    async handleGet(req, res) {
+        const [empty, place] = this.getSubPath(req.url).split('/');
+        try {
+            const { apiRes, data } = await this.callApi(place);
+            res.writeHead(apiRes.statusCode);
+            res.write(data);
+            res.end();
+        } catch(err) {
+            logger.log(`error ${err}`);
             res.writeHead(400);
             res.write(JSON.stringify(err));
             res.end();
-          }
-    });        
-        
+        }
     }
 
     handleRequest(req, res) {
